643ddfbc09658cce3be96fa244d9150985b9fe20
Steponass <steponas.dabuzinskas@gmail.com>
2026-04-10 08:46:17 +0300

    Doob dee doo.

---
+++ src/data/awards.ts
@@ -0,0 +1,39 @@
+export interface Award {
+  icon: string;
+  text: string;
+}
+
+export const AWARDS: Award[] = [
+  { icon: "🥇", text: "2023 Architecture Masterprize (Photography) Awards - Winner" },
+  { icon: "🥇", text: "2023 Architecture Masterprize (Photography) Awards - Winner" },
+  { icon: "🥈", text: "2023 Architecture Masterprize (Photography) Awards - Hon. Mention" },
+  { icon: "🥇", text: "2022 Architecture Masterprize (Photography) Awards - Winner" },
+  { icon: "🥇", text: "2022 Architecture Masterprize (Photography) Awards - Winner" },
+  { icon: "🥇", text: "2022 Architecture Masterprize (Photography) Awards - Winner" },
+  { icon: "🥈", text: "2022 MonoVisions Photography Awards - Hon. Mention" },
+  { icon: "🥈", text: "2022 MIFA (Moscow International Photography Awards) - Hon. Mention" },
+  { icon: "🥈", text: "2021 TIFA (Tokyo International Photography Awards) - Hon. Mention" },
+  { icon: "🥇", text: "2021 Architecture Masterprize (Photography) Awards - Winner" },
+  { icon: "🥈", text: "2021 Architecture Masterprize (Photography) Awards - Hon. Mention" },
+  { icon: "🥇", text: "2021 Architecture Masterprize (Photography) Awards - Winner" },
+  { icon: "🥈", text: "2021 MonoVisions Photography Awards - Hon. Mention" },
+  { icon: "🥈", text: "2021 FAPA (Fine Art Photography Awards) - Nominee Award" },
+  { icon: "🥈", text: "2020 Chromatic (Photography) Awards - Hon. Mention" },
+  { icon: "🥈", text: "2020 Chromatic (Photography) Awards - Hon. Mention" },
+  { icon: "🥈", text: "2020 ND (Neutral Density) Awards - Hon. Mention" },
+  { icon: "🥈", text: "2020 ND (Neutral Density) Awards - Hon. Mention" },
+  { icon: "🥈", text: "2020 IPA (International Photography Awards) - Hon. Mention" },
+  { icon: "🥈", text: "2020 FAPA (Fine Art Photography Awards) - Nominee Award" },
+  { icon: "🥈", text: "2020 FAPA (Fine Art Photography Awards) - Nominee Award" },
+  { icon: "🥈", text: "2020 FAPA (Fine Art Photography Awards) - Nominee Award" },
+  { icon: "🥈", text: "2020 FAPA (Fine Art Photography Awards) - Nominee Award" },
+  { icon: "🥇", text: "2019 The Societies of Photographers - Merit Award" },
+  { icon: "🥇", text: "2019 The Societies of Photographers - Merit Award" },
+  { icon: "🥈", text: "2019 Monochrome Photography Awards - Hon. Mention" },
+  { icon: "🥈", text: "2019 Monochrome Photography Awards - Hon. Mention" },
+  { icon: "🥈", text: "2019 Monochrome Photography Awards - Hon. Mention" },
+  { icon: "🥈", text: "2019 Chromatic Awards - Hon. Mention" },
+  { icon: "🥈", text: "2018 Monochrome Photography Awards - Hon. Mention" },
+  { icon: "🥈", text: "2018 Monochrome Photography Awards - Hon. Mention" },
+  { icon: "🥈", text: "2018 The Photographic Angle - Commended (Award)" },
+];
